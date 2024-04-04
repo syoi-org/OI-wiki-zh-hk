dbff8d1a7171d50dd4f4462d85d36a9b4da02e63
STommydx <dev@stdx.space>
2024-04-04 16:36:41 -0400

    fix: fix theme not being installed

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ pipenv install
 cp -r OI-wiki OI-wiki-zh-hk
 pipenv run python translate.py OI-wiki-zh-hk
 pushd OI-wiki-zh-hk
+./scripts/pre-build/install-theme.sh
 pipenv install
 pipenv run mkdocs build
 popd
